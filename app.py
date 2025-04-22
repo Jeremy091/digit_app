902925136f98d6b369e3f8fba61235910b639f72
Jeremy091 <sheremypavon12@gmail.com>
2025-04-22 01:52:00 -0500

    modelo h5

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# app.py
+import os
 from flask import Flask, render_template, request, jsonify
 from model_utils import predict_image
 import io
@@ -19,4 +19,5 @@ def api_predict():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
